ab1c05716bb5cb3d90f2b0ba9001aabbec23c388
Ruan <ruan@piggy.nl>
2019-07-19 09:30:20 +0200

    Assignment Thu 18 juli 2019

---
+++ my-app/src/Components/Classes/ClassCool.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ClassCool extends React.Component {
+  render() {
+    return <p>Some content returned from a Class - based component.</p>;
+  }
+}
+
+export default ClassCool;
